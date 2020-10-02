1633619297935ef2354919d085ef6eae1aa64e9c
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-01 17:36:25 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-let form = document.getElementById('create-task-form');
-let li = document.createElement('li');
 
 document.addEventListener("DOMContentLoaded", () => {
-  form.addEventListener('submit', function(event) {
-    let userInput = document.getElementById('new-task-description').value;
-    document.getElementById('tasks').innerHTML += `<li>${userInput}</li>`
-    event.preventDefault();
-    form.reset();
-  }
-  );
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', addTask);
 });
+  
+const addTask = function(event) {
+  event.preventDefault();
+  const task = document.getElementById('tasks');
+  const userInput = document.getElementById('new-task-description').value;
+  const newTask = document.createElement('li');
+  newTask.innerHTML = userInput;
+  task.appendChild(newTask);
+  event.target.reset();
+};
