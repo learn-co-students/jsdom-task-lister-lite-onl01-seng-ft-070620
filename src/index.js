25226f328ccbdd9256eb2cf48df4e568e57988a6
JacksonReynolds <jwreynod@gmail.com>
2020-10-06 14:08:30 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  function addTask(e) {
+    e.preventDefault()
+    const newTask = tasks.appendChild(document.createElement('li'))
+    newTask.innerHTML = taskText.value
+    newTask.appendChild(document.createElement('button'))
+    newTask.querySelector('button').innerHTML = 'x'
+    e.target.reset()
+  }
+
+  function deleteTask(e) {
+    e.target.parentElement.remove()
+  }
+  
+  const form = document.querySelector('form')
+  const tasks = document.querySelector('#tasks')
+  const taskText = document.querySelector('#new-task-description')
+
+  form.addEventListener('submit', function(e) {
+    addTask(e)
+  })
+
+  tasks.addEventListener('click', function(e) {
+    deleteTask(e)
+  })
+
 });
