2e0ea5b55761476b524bc45a28f0b5901565377e
jchasse <jonathan.r.chasse@gmail.com>
2020-10-08 14:07:25 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+let taskForm = document.getElementById("create-task-form") // collects the entire form
+let taskShowUl = document.getElementById("tasks"); // show spot for the new Task
+
+
+taskForm.addEventListener("submit", addTaskFunction);
+
+function addTaskFunction(event) {
+  event.preventDefault()
+  let taskText = event.target.children[1].value
+  updateTaskShow(taskText)
+  event.target.reset()
+}
+
+function updateTaskShow(taskText) {
+  let li = document.createElement('li')
+  li.innerHTML = taskText
+  taskShowUl.appendChild(li)
+}
