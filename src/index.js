0763c603100b906ebc05aed3b27efea3473b9686
Ethan <eleetyson@gmail.com>
2020-10-08 16:14:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   let submitBtn = document.querySelector("input[type=submit]")
   submitBtn.addEventListener("click", function(event) {
 
-    // create an li element with the user-inputted text
-    let task = document.createElement("li")
-    let text = document.getElementById("new-task-description").value
-    task.innerHTML = text
-
-    // add the new li element as a child to the ul
-    let list = document.querySelector("ul#tasks")
-    list.appendChild(task)
-
     // prevent the default request made upon form submission
     event.preventDefault()
+
+    let list = document.querySelector("ul#tasks") // grab the ul
+    let task = document.createElement("li") // create a new li
+    // grab the user inputted-text
+    let text = document.getElementById("new-task-description").value
+
+    task.innerHTML = text // set HTML for the li equal to user's input
+    list.appendChild(task) // add the new li element as a child to the ul
+
   })
 
 })
